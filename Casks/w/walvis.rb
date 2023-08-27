b8a9673de6b39bf12bb5198075eb6fc20f8df56d
Federico Ruggi <federico.ruggi@hey.com>
2023-08-27 20:48:25 +0200

    first commit

---
+++ Casks/w/walvis.rb
@@ -0,0 +1,23 @@
+cask 'walvis' do
+  name "Walvis"
+  desc "Manage Docker containers from the menu bar."
+  homepage "https://github.com/ruggi/walvis"
+
+  version "1.12.3"
+  sha256 "376d018f0d29b4557ef9ac402c88cf45a43011e8f150de5bc94cd0275f8a54f5"
+
+  url "https://github.com/ruggi/walvis/releases/download/v#{version}/Walvis-#{version}.zip"
+
+  app "Walvis.app"
+  auto_updates true
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+(?:-beta\.?\d*)?)$/i)
+  end
+
+  zap trash: [
+    "~/Library/LaunchAgents/com.gihtub.ruggi.walvis",
+    "~/Library/Preferences/com.github.ruggi.walvis.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# tap
+
+Custom tap for my Homebrew formulae 🍺
+
+## How do I install these formulae?
+
+`brew install ruggi/tap/<formula>`
+
+Or `brew tap ruggi/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
